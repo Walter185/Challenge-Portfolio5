4c70d2dd2c9a5fdad8db270c0da0bd99e9494e00
Walter Liendo <walter.liendo@gmail.com>
2023-06-25 00:34:24 -0300

    agregue proyectos y comence la validacion del formulario

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -1,11 +1,22 @@
 var switchLangButton = document.getElementById('switch-lang');
 var titleElement = document.getElementById('title');
 var hobbiesElement = document.getElementById('hobbies');
+var hobbies2Element = document.getElementById('hobbies2');
+var projects2Element = document.getElementById('projects2'); 
 var trainingElement = document.getElementById('training');
+var training2Element = document.getElementById('training2');
+
 var projectsElement = document.getElementById('projects');
 var contactElement = document.getElementById('contact');
 var aboutElement = document.getElementById('about');
 var skillsElement = document.getElementById('skills');
+var skills2Element = document.getElementById('skills2');
+
+var downloadElement = document.getElementById('download');
+var presentacionElement = document.getElementById('presentacion');
+var about2Element = document.getElementById('about2');
+var about3Element = document.getElementById('about3');
+var about4Element = document.getElementById('about4');
 
 var currentLang = 'es';
 
@@ -14,11 +25,21 @@ switchLangButton.addEventListener('click', function() {
     currentLang = 'en';
     titleElement.textContent = 'Hello!, my name is Walter and I do web development.';
     hobbiesElement.textContent = 'Hobbies';
+    hobbies2Element.textContent = 'Hobbies';
     trainingElement.textContent = 'Training';
     aboutElement.textContent = 'About me';
     projectsElement.textContent = 'Projects';
     contactElement.textContent = 'Contact';
     skillsElement.textContent = 'Skills';
+    skills2Element.textContent = 'Skills';
+    training2Element.textContent = 'Academic Training';
+    projects2Element.textContent = 'Projects';
+
+    downloadElement.textContent = 'Download CV';
+    presentacionElement.textContent = 'I am Full-Stack and currently participating in Alura s Oracle ONE project. In addition, I am specializing in Cybersecurity with IBM SkillSBuild.';
+    about2Element.textContent = 'About me';
+    about3Element.textContent = 'Passionate about technology and everything that one can create with it. Unstoppable in the search for new tools that can make my work more appreciable for my clients.';
+    about4Element.textContent = 'I am currently working freelance, but I am open to a part-time or full-time asynchronous position.';
 
     switchLangButton.textContent = 'Cambiar Idioma';
   } else {
@@ -26,11 +47,20 @@ switchLangButton.addEventListener('click', function() {
     titleElement.textContent = 'Hola!, mi nombre es Walter y hago desarrollo web.';
     aboutElement.textContent = 'Acerca de mi';
     hobbiesElement.textContent = 'Pasatiempos';
+    hobbies2Element.textContent = 'Pasatiempos';
     projectsElement.textContent = 'Proyectos';
     trainingElement.textContent = 'Formación';
     contactElement.textContent = 'Contacto';
     skillsElement.textContent = 'Habilidades';
+    training2Element.textContent = 'Formación Académica';
+    projects2Element.textContent = 'Proyectos';
 
+    skills2Element.textContent = 'Habilidades';
+    downloadElement.textContent = 'Descargar CV';
+    presentacionElement.textContent = 'Soy Full-Stack y actualmente participando del proyecto Oracle ONE de Alura. Además, me estoy especializando en CyberSeguridad con IBM SkillSBuild.';
+    about2Element.textContent = 'Acerca de mi';
+    about3Element.textContent = 'Apasionado por la tecnologia y todo lo que uno puede llegar a crear con ella. Imparable en la búsqueda de nuevas herramientas que puedan hacer mi labor más apreciable por mis clientes.';
+    about4Element.textContent = 'Actualmente me encuentro trabajando con forma freelance, pero me encuentro abierto a un puesto part-time o full-time asincrono.'
     switchLangButton.textContent = 'Switch Language';
   }
 });
--- assets/js/validacion.js
+++ assets/js/validacion.js
@@ -1,1 +1,175 @@
-//Haz tú validación en javascript acá
+// Contact form validations
+const form = document.querySelector(".contact__form");
+const formInputs = document.querySelectorAll(".form__input");
+
+const states = {
+  VALID: "valid",
+  INVALID: "invalid",
+};
+
+const constraints = {
+  name: {
+    required: true,
+    errorMessages: {
+      required: "Name field cannot be empty",
+    },
+  },
+  email: {
+    required: true,
+    regex:
+      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+    errorMessages: {
+      required: "Email field cannot be empty",
+      regex: "Email format is invalid",
+    },
+  },
+  subject: {
+    required: true,
+    regex: /^\w{2,}/i,
+    errorMessages: {
+      required: "Subject field cannot be empty",
+      regex: "Subject  must be a word with at least 2 letters",
+    },
+  },
+  message: {
+    required: true,
+    minLength: 10,
+    errorMessages: {
+      required: "Message field cannot be empty",
+      minLength: "Message must be at least 10 characters long",
+    },
+  },
+};
+
+const setClassName = ({ node, className }) => {
+  node.classList.add(className);
+};
+
+const removeClassName = ({ node, className }) => {
+  node.classList.remove(className);
+};
+
+const renderInlineError = ({ errorElement, error }) => {
+  errorElement.innerHTML = `
+    <span class="visually-hidden">Error: </span>
+    ${error}
+  `;
+};
+
+const clearInlineError = ({ errorElement }) => {
+  errorElement.innerText = "";
+};
+
+const clearValidationStates = ({ node }) => {
+  Object.values(states).forEach((state) => {
+    removeClassName({ node, className: state });
+  });
+};
+
+const renderValidationState = ({ node, state }) => {
+  clearValidationStates({ node });
+
+  if (state === states.INVALID) {
+    setClassName({ node, className: states.INVALID });
+  } else {
+    setClassName({ node, className: states.VALID });
+  }
+};
+
+const validateField = ({ name, value }) => {
+  const { required, regex, minLength, errorMessages } = constraints[name];
+  let isValid = true;
+  let error = "";
+
+  if (required && !value) {
+    error = errorMessages.required;
+  } else if (regex && !regex.test(value)) {
+    error = errorMessages.regex;
+  } else if (minLength && value.length < minLength) {
+    error = errorMessages.minLength;
+  }
+
+  if (error) {
+    isValid = false;
+  }
+
+  return { isValid, error };
+};
+
+const checkInput = ({ field }) => {
+  const { name, value } = field;
+  const formItem = field.closest(".form__item");
+
+  const { isValid, error } = validateField({ name, value });
+
+  if (isValid) {
+    field.setAttribute("aria-invalid", false);
+    renderValidationState({ node: formItem, state: states.VALID });
+  } else {
+    field.setAttribute("aria-invalid", true);
+    renderValidationState({ node: formItem, state: states.INVALID });
+  }
+
+  return { error };
+};
+
+const validateHoneypot = () => {
+  const isFillOut = document.getElementById("honeypot").value;
+
+  if (isFillOut) return true;
+
+  return false;
+};
+
+const validateForm = () => {
+  let isValid = true;
+
+  [...formInputs].forEach((input) => {
+    const { error } = checkInput({ field: input });
+
+    if (error) isValid = false;
+  });
+
+  return isValid;
+};
+
+form.addEventListener("focusout", (e) => {
+  if (e.target.tagName !== "INPUT" && e.target.tagName !== "TEXTAREA") return;
+
+  const { error } = checkInput({ field: e.target });
+  const errorElement = document.getElementById(`${e.target.name}-error`);
+
+  if (error) {
+    renderInlineError({ errorElement, error });
+    return;
+  }
+
+  clearInlineError({ errorElement });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const isBotxD = validateHoneypot();
+  if (isBotxD) {
+    console.log("You're a bot 🤖. Hahaha! Or not?");
+    return;
+  }
+
+  const isValid = validateForm();
+
+  if (!isValid) {
+    const firstErrorElement = document.querySelector("[aria-invalid=true]");
+    firstErrorElement.focus();
+    return;
+  }
+
+  const formItems = document.querySelectorAll(".form__item");
+  formItems.forEach((node) => {
+    clearValidationStates({ node });
+  });
+
+  form.reset();
+
+  console.log("Form sent successfully!");
+});
